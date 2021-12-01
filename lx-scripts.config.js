cc434c6465d51ff75dfe9c0e398f223871fde3ae
tjwyz <huntlian@tencent.com>
2021-12-01 21:47:43 +0800

    1

--- lx-scripts.config.js
+++ lx-scripts.config.js
@@ -1,9 +1,7 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    cdnPrefix: process.env.USE_LOCAL_ASSETS === 'true' || process.env.NODE_ENV !== 'production'
-        ? '/'
-        : '//ali.static.yximgs.com',
+    cdnPrefix: '/',
     assetsDir: 'static/luye',
     chainWebpack(config) {
         if (process.env.GEN_WEBPACK_BUNDLE_ANALYZER === 'true') {
